d392f6ab68b29a4141bff86669e699f1e4176df3
Bradley Guthrie <guppy96@gmail.com>
2020-10-01 20:24:52 -0500

    brining in view

--- vanillajs/app.js
+++ vanillajs/app.js
@@ -1,14 +1,19 @@
-const button = document.querySelector('button');
-const input = document.querySelector('input');
-const list = document.querySelector('ul');
 
 
-function addGoal() {
-  const enteredValue = input.value;
-  const listItem = document.createElement('li');
-  listItem.textContent = enteredValue;
-  list.appendChild(listItem);
-  input.value = '';
-}
 
-button.addEventListener('click', addGoal);
+
+
+// const button = document.querySelector('button');
+// const input = document.querySelector('input');
+// const list = document.querySelector('ul');
+
+
+// function addGoal() {
+//   const enteredValue = input.value;
+//   const listItem = document.createElement('li');
+//   listItem.textContent = enteredValue;
+//   list.appendChild(listItem);
+//   input.value = '';
+// }
+
+// button.addEventListener('click', addGoal);
