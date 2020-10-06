041fbee47c5c8bcf7bb32e9a1ad8a50199d70e69
Bradley Guthrie <guppy96@gmail.com>
2020-10-05 19:21:16 -0500

    first method with vue

--- instance/app.js
+++ instance/app.js
@@ -5,6 +5,16 @@ const app = Vue.createApp({
             messageGoal: 'Learn Vue',
             vueLink: 'https://vueJs.org'
         };
+    },
+    methods: {
+        outputMessage: function () {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'You Need to learn Vue!';
+            } else {
+                return 'You will master Vue!';
+            }
+        }
     }
 });
 
