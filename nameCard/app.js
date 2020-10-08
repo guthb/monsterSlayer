7b3e22524cda88f44d2c748a614ce171d67b34b9
Bradley Guthrie <guppy96@gmail.com>
2020-10-08 06:27:16 -0500

    used v-bind to display image stored in data object

--- nameCard/app.js
+++ nameCard/app.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
   data() {
     return {
       name: 'Vue Developer',
-      age: 25
+      age: 25,
+      imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.UH0Im37AH1OJSew-57Bk8AHaE6%26pid%3DApi&f=1'
     };
   },
   methods: {
