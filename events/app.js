50ffc1354a379c729a1da48ff508345cf33a430b
Bradley Guthrie <guppy96@gmail.com>
2020-10-09 06:28:40 -0500

    calling method by just name

--- events/app.js
+++ events/app.js
@@ -1,9 +1,24 @@
+//JS 
+//document.querySelector('button').addEventListener();
+
+
+// @ts-ignore
 const app = Vue.createApp({
   data() {
     return {
       counter: 0,
     };
   },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+
+    decrement() {
+      this.counter--;
+    }
+
+  }
 });
 
 app.mount('#events');
