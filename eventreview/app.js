bee818c021718a4543cfb4a4c2749028bc13756e
Bradley Guthrie <guppy96@gmail.com>
2020-10-11 13:00:37 -0500

    user input displayed with delay from using keydown

--- eventreview/app.js
+++ eventreview/app.js
@@ -1,8 +1,14 @@
 // @ts-nocheck
 const app = Vue.createApp({
+    data() {
+        return { userInput: '' }
+    },
     methods: {
         displayAlert() {
             alert('Alert Alert')
+        },
+        registerUser(event) {
+            this.userInput = event.target.value;
         }
     }
 });
