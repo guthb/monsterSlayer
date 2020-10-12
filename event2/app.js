3f6d587d0b5328e03aa236360beae3f3f9b558ef
Bradley Guthrie <guppy96@gmail.com>
2020-10-12 07:08:27 -0500

    computed property for last name population

--- event2/app.js
+++ event2/app.js
@@ -9,6 +9,12 @@ const app = Vue.createApp({
     setName(event, lastName) {
       this.name = event.target.value;
     },
+    fullnameOutput() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Smiley';
+    },
     add(num) {
       this.counter = this.counter + num;
     },
@@ -16,7 +22,15 @@ const app = Vue.createApp({
       this.counter = this.counter - num;
     },
     resetInput() {
-      this.name = ""
+      this.name = "";
+    }
+  },
+  computed: {
+    fullname() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Smiley';
     }
   }
 });
