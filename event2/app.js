42dc62eccb78a1b6ecd1cfd5d25fdaec4893bed3
Bradley Guthrie <guppy96@gmail.com>
2020-10-13 06:58:41 -0500

    watcher based on timer

--- event2/app.js
+++ event2/app.js
@@ -28,6 +28,18 @@ const app = Vue.createApp({
     }
   },
   watch: {
+    counter(value) {
+      if (value > 50) {
+
+        const that = this;
+
+        setTimeout(function () {
+          // @ts-ignore
+          that.counter = 0;
+        }, 2000);
+
+      }
+    }
     // name(value) {
     //   if (value === '') {
     //     this.fullname = '';
