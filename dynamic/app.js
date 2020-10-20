63a2ecdec8993c546b757745f6255be6dea1fc3b
Bradley Guthrie <guppy96@gmail.com>
2020-10-19 20:22:12 -0500

    unable to get the toggle to hide or make visible the paragraph tag

--- dynamic/app.js
+++ dynamic/app.js
@@ -1,8 +1,24 @@
 const app = Vue.createApp({
     data() {
         return {
-            inputClass: ''
+            inputClass: '',
+            paragraphIsVisible: true
         };
+    },
+    computed: {
+        paragraphClasses() {
+            return {
+                user1: this.inputClass === 'user1',
+                user2: this.inputClass === 'user2',
+                visible: this.paragraphIsVisiable,
+                hidden: !this.paragraphIsVisiable
+            }
+        }
+    },
+    methods: {
+        toggleParagraphVisibility() {
+            this.paragraphIsVisible = !this.paragraphIsVisible;
+        }
     }
 });
 app.mount("#dynamic");
