09281c2cfaeff2a03e76b56431572adc55510874
Bradley Guthrie <guppy96@gmail.com>
2020-11-16 17:38:31 -0600

    props as object with type definitions

--- newvueproject/src/App.vue
+++ newvueproject/src/App.vue
@@ -12,7 +12,6 @@
              name='Julie Jones'
              phone-number='0123 45678 90'
              email-address='julie@localhost.com'
-             is-favorite="0"
             ></friend-contact>
         </ul>
     </section>
