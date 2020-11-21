df9714652193fb5c788f82564b2074e8e25e0f7c
Bradley Guthrie <guppy96@gmail.com>
2020-11-21 07:24:23 -0600

    added delete to form

--- newvueproject/src/App.vue
+++ newvueproject/src/App.vue
@@ -11,7 +11,8 @@
                 :phone-number="friend.phone"
                 :email-address="friend.email"
                 :is-favorite="friend.isFavorite"
-                @toggle-favorite="toggleFavoriteStatus"               
+                @toggle-favorite="toggleFavoriteStatus"
+                @delete="deleteContact"               
             ></friend-contact>
         </ul>
     </section>
@@ -55,6 +56,9 @@ export default {
               isFavorite: false
             };
           this.friends.push(newFriendContact);
+        },
+        deleteContact(friendId){
+          this.friends = this.friends.filter(friend => friend.id !== friendId);
         }
     }  
 }
