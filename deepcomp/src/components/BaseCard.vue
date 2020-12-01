0a1ddaa1322b530a92c61f19bca54b17c5949bac
Bradley Guthrie <guppy96@gmail.com>
2020-12-01 07:57:15 -0600

    shorthand for slots and testing for empty header errors on components

--- deepcomp/src/components/BaseCard.vue
+++ deepcomp/src/components/BaseCard.vue
@@ -1,7 +1,9 @@
 <template>
     <section>
-        <header>
-          <slot name="header"></slot>
+        <header v-if="$slots.header">
+          <slot name="header">
+            <!-- <h2>The Default</h2> -->
+          </slot>
         </header>
         <slot></slot>
     </section>
@@ -9,6 +11,7 @@
 
 <script>
 export default {
+  mounted(){console.log(this.$slots);}
 }
 </script>
 
