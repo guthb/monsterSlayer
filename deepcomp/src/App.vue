8e535f3dd96f1571f4e2bbc4bd9cfe50ca8595fb
Bradley Guthrie <guppy96@gmail.com>
2020-12-03 15:47:33 -0600

    dynamic comp, had issue and corrected when property was defined outside another property

--- deepcomp/src/App.vue
+++ deepcomp/src/App.vue
@@ -1,38 +1,45 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <!-- <badge-list></badge-list> -->
+
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent"></component>
+    <!-- <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
-    <course-goals #default="slotProps"  >
+    <course-goals #default="slotProps"  > -->
       <!-- <template #default="slotProps"> -->
-      <h2>{{ slotProps.item}}</h2>
-      <p>{{ slotProps['another-prop']}}</p>
+      <!-- <h2>{{ slotProps.item}}</h2>
+      <p>{{ slotProps['another-prop']}}</p> -->
       <!-- </template> -->
-    </course-goals>
+    <!-- </course-goals> -->
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
-
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGols.vue';
+import ManageGoals from './components/MagangedGoals.vue';
 
 export default {
 
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals
+    ActiveGoals,
+    ManageGoals
   },
   data() { 
       return {
+        selectedComponent: 'active-goals',
       activeUser: {
         name: 'Joe Black',
         description: "Soul Taker",
@@ -40,6 +47,12 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+      console.log("button pressed" , cmp)
+    }
+  }
 };
 </script>
 
