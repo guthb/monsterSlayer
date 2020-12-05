ee1696981b976f7459a5784e21a579cc06e63b3c
Bradley Guthrie <guppy96@gmail.com>
2020-12-05 11:10:48 -0600

    stying is off

--- deepcomp/src/components/MagangedGoals.vue
+++ deepcomp/src/components/MagangedGoals.vue
@@ -3,11 +3,13 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal"/>
     <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
     <error-alert v-if="inputIsInvalid">
         <h2>Input is invalid</h2>
         <p> Please enter at least a few charachters</p>
-        <button @click="confrimError">Okay</button>
+        <button @click="confirmError">Okay</button>
     </error-alert>
+    </teleport>
 </div>
 </template>
 
@@ -32,7 +34,7 @@ export default {
 
             }
         },
-        confrimError() {
+        confirmError() {
             this.inputIsInvalid = false;
         }
     }
