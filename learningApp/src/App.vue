d4c47c37a6652a26c74d2f936648baa4927d638b
Bradley Guthrie <guppy96@gmail.com>
2020-12-07 18:02:48 -0600

    list built

---
+++ learningApp/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul></ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                { 
+                    id: 'official-guide', 
+                    title: 'Official Guide', 
+                    description: 'offical Vue.js docs',
+                    link: 'https://vuejs.org'
+
+                },
+                { 
+                    id: 'google', 
+                    title: 'Google', 
+                    description: 'google this...',
+                    link: 'https://google.com'
+
+                },
+            ],
+        };
+    },
+}
+</script>
