fdf4385097b9953a07abd38692f917a8dd581a2f
Bradley Guthrie <guppy96@gmail.com>
2020-12-09 06:14:46 -0600

    styled app and learingresource comp

--- learningApp/src/App.vue
+++ learningApp/src/App.vue
@@ -1,40 +1,55 @@
 <template>
-    <ul>
-        <learning-resource v-for="res in storedResources" 
-        :key="res.id" 
-        :title="res.title"
-        :description="res.description"
-        :link="res.link"
-        ></learning-resource>
-    </ul>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue'
+import LearningResource from './components/learning-resources/LearningResource.vue';
 
 export default {
-    components:{
-        LearningResource
-    },
-    data() {
-        return {
-            storedResources: [
-                { 
-                    id: 'official-guide', 
-                    title: 'Official Guide', 
-                    description: 'offical Vue.js docs',
-                    link: 'https://vuejs.org'
-
-                },
-                { 
-                    id: 'google', 
-                    title: 'Google', 
-                    description: 'google this...',
-                    link: 'https://google.com'
-
-                },
-            ],
-        };
-    },
-}
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'offical Vue.js docs',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'google this...',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  }
+};
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
