84a68b8ab552b51ca7f6ff0f543b54d56476d599
Bradley Guthrie <guppy96@gmail.com>
2020-12-12 07:20:35 -0600

    need a dynamic prop

---
+++ learningApp/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,33 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resources')"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResouces from './StoredResource.vue';
+import AddResources from './AddResources.vue';
+
+export default {
+  components: {
+    StoredResouces,
+    AddResources
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources'
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    }
+  }
+};
+</script>
