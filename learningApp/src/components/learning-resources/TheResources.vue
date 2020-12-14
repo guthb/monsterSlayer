7b1c8fd30b67946136909bf6f47ef95304e66bb2
Bradley Guthrie <guppy96@gmail.com>
2020-12-14 11:45:43 -0600

    added keep alive and allowed user input

--- learningApp/src/components/learning-resources/TheResources.vue
+++ learningApp/src/components/learning-resources/TheResources.vue
@@ -4,7 +4,9 @@
       >Stored Resources</base-button>
     <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -37,7 +39,8 @@ export default {
   },
   provide() {
     return {
-      resources: this.storedResources
+      resources: this.storedResources,
+      addResource: this.addResource
     };
   },
   computed:{
@@ -52,6 +55,18 @@ export default {
     setSelectedTab(tab) {
       this.selectedTab = tab;
     },
+    addResource(title, description, url)
+    {
+      const newResource =
+      {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url
+      }
+      this.storedResources.unshift(newResource);
+      this.selectedTab='stored-resource';
+    }
   },
 };
 </script>
