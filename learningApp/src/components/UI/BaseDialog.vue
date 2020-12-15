affb1b75734aa6de49c0d45a4793d88b21cb7793
Bradley Guthrie <guppy96@gmail.com>
2020-12-15 08:00:05 -0600

    building a modal

---
+++ learningApp/src/components/UI/BaseDialog.vue
@@ -0,0 +1,28 @@
+<template>
+    <dialog open>
+        <header>
+            <slot name="header">
+                <h2>{{title}}</h2>
+            </slot>
+        </header>
+        <section>
+            <slot></slot>
+        </section>
+        <menu>
+           <slot name="actions"></slot> 
+        </menu>
+    </dialog>
+</template>
+
+<script>
+import MagangedGoals from '../../../../deepcomp/src/components/MagangedGoals.vue'
+export default {
+  components: { MagangedGoals },
+    props:{
+        title: {
+            type: String,
+            required: false
+        }
+    }        
+}
+</script>
