d22c7e5d426b82023a0caa57ee4c53f1ec813b70
Bradley Guthrie <guppy96@gmail.com>
2020-12-23 06:08:31 -0600

    unordered list and list items styled

--- form/src/components/RatingControl.vue
+++ form/src/components/RatingControl.vue
@@ -1,7 +1,25 @@
 <template>
     <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li><button>Poor</button></li>
+        <li><button>Average</button></li>
+        <li><button>Great</button></li>
     </ul>
 </template>
+
+<style  scoped>
+ul {
+    list-style: none;
+    margin: 0.5rem 0;
+    padding: 0;
+    display: flex;
+}
+
+li {
+    margin: 0 1rem;
+    border: 1 px solid #ccc;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-itmes: center;
+}
+</style>
