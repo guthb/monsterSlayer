c6e23e35b43b9276e192a09f84a4053450290890
Bradley Guthrie <guppy96@gmail.com>
2021-01-09 08:35:44 -0600

    can't read prop thought routing

--- routing/src/components/teams/TeamMembers.vue
+++ routing/src/components/teams/TeamMembers.vue
@@ -18,6 +18,7 @@ import UserItem from '../users/UserItem.vue';
 
 export default {
   inject: [users, teams],
+  props:['teamId'],
   components: {
     UserItem
   },
@@ -33,8 +34,9 @@ export default {
     };
   },
   methods: {
-    loadTeamMembers(route) {
-      const teamId = route.params.teamId;
+    //loadTeamMembers(route) {
+    loadTeamMembers(teamId) {
+      //const teamId = route.params.teamId;
       const selectedTeam = this.teams.find(team => team.id == teamId);
       const members = selectedTeam.members;
       const selectedMembers = []
@@ -47,11 +49,15 @@ export default {
     },
   },
   created() {
-    this.loadTeamMembers(this.$route)
+    //this.$route.path// / teams/t1
+    //this.loadTeamMembers(this.$route)
+    this.loadTeamMembers(this.teamId)
   },
   watch: {
-    $route(newRoute){
-      this.loadTeamMembers(newRoute)
+    
+    //$route(newRoute){
+    teamId(newId){
+      this.loadTeamMembers(newId)
     },
   },
 };
