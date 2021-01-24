1da9b444d23fec0414f8ff53a5137c752f16df10
Bradley Guthrie <guppy96@gmail.com>
2021-01-24 10:13:05 -0600

    adding vue helper classes to css

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -3,6 +3,12 @@
     <div class="block" :class="{animate: animatedBlock}"></div>
     <button @click="animateBlock">Animate</button>
   </div>
+  <div>
+    <transition>
+      <p v-if="paraIsVisible">This is testing paragraph visibility</p>
+    </transition> 
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -80,6 +86,34 @@ button:active {
 .animate {
   /* transform: translateX(-150px); */
   animation: slide-fade .3s ease-out forwards;
+}
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
 }
 
 @keyframes slide-fade {
