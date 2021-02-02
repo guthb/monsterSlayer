5e445b30370ae697462d8f8430bea502b5ac1a49
Bradley Guthrie <guppy96@gmail.com>
2021-02-01 19:03:22 -0600

    styling added

--- animations/src/components/UserList.vue
+++ animations/src/components/UserList.vue
@@ -36,10 +36,35 @@ export default {
     list-style: none;
     margin: 1rem 0;
     padding: 0;
-  }
+  } 
+
   li {
     border: 1px solid #ccc;
     padding: 1rem;
     text-align: center;
+  }
+
+  .user-list-enter-from {
+    opacity: : 0;
+    transform: translateX(-30px);
+  }
+
+  .user-list-enter-active {
+    transition: all 1s ease-out;;
+  }
+
+  .user-list-enter-to,
+  .user-list-leave-from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+
+  .user-list-leave-active {
+    transition: all 1s ease-in;;
+  }
+
+  .user-list-enter-from {
+    opacity: : 0;
+    transform: translateX(30px);
   }
 </style>
