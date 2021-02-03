c09424b2cf30600532d9a51ed37f698f2e41d102
Bradley Guthrie <guppy96@gmail.com>
2021-02-03 07:42:17 -0600

    cleaning up animation

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <transition>
-    <router-view></router-view>
-  </transition>
+  <router-view v-slot="slotProps">   
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
 </template>  
 
 <script>
