c5c828be3b170c244bf01646a107070c1f2b80aa
Bradley Guthrie <guppy96@gmail.com>
2021-02-10 06:10:55 -0600

    multiple getter for value normalization

--- state/src/main.js
+++ state/src/main.js
@@ -22,7 +22,17 @@ const store = createStore({
     getters: {
         finalCounter(state) {
             return state.counter * 2;
-         }
+        },
+        normalizedCounter(state) { 
+            if (finalCounter < 0) { 
+                return 0
+            }
+            if (finalCounter > 100) { 
+                return 100;
+            }
+            return finalCounter;
+        
+        }
     }
 })
 
