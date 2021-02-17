e59b22e9d1574335f57a8c9b87fa7f26e80b3d5e
Bradley Guthrie <guppy96@gmail.com>
2021-02-17 06:42:46 -0600

    added store file

--- state/src/main.js
+++ state/src/main.js
@@ -1,87 +1,9 @@
 import { createApp } from 'vue';
 
-import {createStore} from 'vuex'
-
 import App from './App.vue';
+import store from './store.js'
 
-const counterModule = {
-    namespaced: true,
-    state() { 
-        return {
-             counter: 0,
-           
-        };
-    },
-    mutations: {
-        increment(state) { 
-            console.log(state)
-            state.counter = state.counter + 2; //has to be snyc
-        },
-        increase(state, payload) { 
-            state.counter = state.counter + payload.value;
-        }, 
-    },
-    actions: {
-        increment(context) { 
-            setTimeout(function () {
-                context.commit('increment')  //async
-            }, 2000);
-        },
-        increase(context, payload) { 
-            context.commit('increase', payload)
-        },
-    },
-    getters: {
-        testauth(state) { 
-            return state.isLoggedIn;
-        },
-        finalCounter(state) {
-            return state.counter * 3;
-        },
-        normalizedCounter(_, getters) { 
-            const finalCounter = getters.finalCounter;
-            if (finalCounter < 0) { 
-                return 0
-            }
-            if (finalCounter > 100) { 
-                return 100;
-            }
-            return finalCounter;
-        
-        },
-    }
-}
 
-const store = createStore({
-    modules: {
-        counter: counterModule
-    },
-    state() { 
-        return {
-            isLoggedIn: false
-        };
-    },
-    mutations: {
-           setAuth(state, payload) { 
-            state.isLoggedIn = payload.isAuth;
-        }   
-    },
-    actions: {
-        
-        login(context) { 
-            context.commit('setAuth',{ isAuth: true });
-        },
-        logout(context) { 
-            context.commit('setAuth', { isAuth: false });
-        },
-    },
-    getters: {
-       
-        userIsAuthenticated(state) {
-            return state.isLoggedIn;
-         }
-    }
-})
 
 const app = createApp(App);
 
---
+++ state/src/store.js
@@ -0,0 +1,82 @@
+import {createStore} from 'vuex'
+
+const counterModule = {
+    namespaced: true,
+    state() { 
+        return {
+             counter: 0,
+           
+        };
+    },
+    mutations: {
+        increment(state) { 
+            console.log(state)
+            state.counter = state.counter + 2; //has to be snyc
+        },
+        increase(state, payload) { 
+            state.counter = state.counter + payload.value;
+        }, 
+    },
+    actions: {
+        increment(context) { 
+            setTimeout(function () {
+                context.commit('increment')  //async
+            }, 2000);
+        },
+        increase(context, payload) { 
+            context.commit('increase', payload)
+        },
+    },
+    getters: {
+        testauth(state) { 
+            return state.isLoggedIn;
+        },
+        finalCounter(state) {
+            return state.counter * 3;
+        },
+        normalizedCounter(_, getters) { 
+            const finalCounter = getters.finalCounter;
+            if (finalCounter < 0) { 
+                return 0
+            }
+            if (finalCounter > 100) { 
+                return 100;
+            }
+            return finalCounter;
+        
+        },
+    }
+}
+
+const store = createStore({
+    modules: {
+        counter: counterModule
+    },
+    state() { 
+        return {
+            isLoggedIn: false
+        };
+    },
+    mutations: {
+           setAuth(state, payload) { 
+            state.isLoggedIn = payload.isAuth;
+        }   
+    },
+    actions: {
+        
+        login(context) { 
+            context.commit('setAuth',{ isAuth: true });
+        },
+        logout(context) { 
+            context.commit('setAuth', { isAuth: false });
+        },
+    },
+    getters: {
+       
+        userIsAuthenticated(state) {
+            return state.isLoggedIn;
+         }
+    }
+})
+
+export default store;
