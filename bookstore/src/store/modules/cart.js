57f4f72f861e5beab84e920b0ea780a4211d6787
Bradley Guthrie <guppy96@gmail.com>
2021-02-20 07:54:42 -0600

    cart and product item converted

--- bookstore/src/store/modules/cart.js
+++ bookstore/src/store/modules/cart.js
@@ -9,7 +9,8 @@ export default {
     },
     mutations: {
         addProductToCart(state, payload) {
-            const productData = payload.product
+            
+            const productData = payload
             const productInCartIndex = state.items.findIndex(
               (ci) => ci.productId === productData.id
             );
@@ -43,7 +44,10 @@ export default {
     },
     actions: {
         addToCart(context, payload){
-            context.commit('addProductToCart', payload);
+          const prodId = payload.id;
+          context.rootGetters['prods/products'];
+          const product = products.find(prod => prod.id === prodId);
+          context.commit('addProductToCart', product);
         },
         removeFromCart(context,payload){ 
             context.commit('removeProductFromCart', payload);
@@ -54,7 +58,7 @@ export default {
             return state.items;
         },
         totalSum(state){
-            return state.total;
+            return state.total.toFixed(2);
         },
         quantity(state){
             return state.qnty;
