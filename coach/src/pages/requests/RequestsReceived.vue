d8a3c9f7c1b66e1075091eb77a7a68006ec0b54c
Bradley Guthrie <guppy96@gmail.com>
2021-02-26 12:16:02 -0600

    corrected error with route path

--- coach/src/pages/requests/RequestsReceived.vue
+++ coach/src/pages/requests/RequestsReceived.vue
@@ -1,3 +1,3 @@
 <template>
-    REQUESTs
+  REQUESTS
 </template>
--- coach/src/router.js
+++ coach/src/router.js
@@ -1,6 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetails.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration';
 import ContactCoach from './pages/requests/ContactCoach.vue';
@@ -13,7 +13,7 @@ const router = createRouter({
     { path: '/', redirect: '/coaches'},
     { path: '/coaches', component: CoachesList},
     { 
-      path: '/coaches/id:', 
+      path: '/coaches/:id', 
       component: CoachDetail, 
       children: [
         { path: 'contact', component: ContactCoach}, // /coaches/c1/contact
