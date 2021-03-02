8653d807d2c73a31735e169d4352af7372e59845
Bradley Guthrie <guppy96@gmail.com>
2021-03-02 06:56:26 -0600

    button comp created and added changing to this comp

--- coach/src/pages/coaches/CoachesList.vue
+++ coach/src/pages/coaches/CoachesList.vue
@@ -5,8 +5,8 @@
     <section>
         <base-card>
         <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
+            <base-button>Refresh</base-button>
+            <base-button link to="/register">Register as Coach</base-button>
         </div>
         <ul v-if="hasCoaches">
             <!-- <li v-for="coach in filteredCoaches" :key ="coach.id">
