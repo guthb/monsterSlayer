0e4da092fc604761e782d408967d0cc685e813c7
Bradley Guthrie <guppy96@gmail.com>
2021-03-02 07:41:16 -0600

    styling completed, no badges yet

--- coach/src/main.js
+++ coach/src/main.js
@@ -5,6 +5,7 @@ import store from './store/index.js'
 import App from './App.vue';
 import BaseCard from './components/ui/BaseCard.vue'
 import BaseButton from './components/ui/BaseButton.vue'
+import BaseBadge from './components/ui/BaseBadge.vue'
 
 const app = createApp(App)
 
@@ -12,7 +13,8 @@ app.use(router);
 app.use(store);
 
 app.component('base-card', BaseCard);
-app.component('base-button', BaseButton)
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
 
