789c2df39e9b32533b152d2d50b96f517c8caddc
Bradley Guthrie <guppy96@gmail.com>
2021-03-06 07:10:39 -0600

    form template wrapped up

--- coach/src/components/coaches/CoachForm.vue
+++ coach/src/components/coaches/CoachForm.vue
@@ -9,8 +9,27 @@
             <input type="text" id="lastname"/>
         </div>
         <div class="form-control">
-            <label for="caareer">Career</label>
-            <input type="text" id="career"/>
+            <label for="description">Description</label>
+            <textarea id="description" rows="5"></textarea>
+        </div>
+         <div class="form-control">
+            <label for="rate">Hourly Rate</label>
+            <input type="number" id="rate"/>
+        </div>
+        <div class="form-control">
+            <h3>Areas of Expertise</h3>
+            <div>
+                <input type="checkbox" id="frontend" value="frontend">
+                <lable for="frontend">Frontend Development</lable>
+            </div>
+             <div>
+                <input type="checkbox" id="backend" value="backend">
+                <lable for="backend">Backend Development</lable>
+            </div>
+             <div>
+                <input type="checkbox" id="career" value="career">
+                <lable for="career">Career Advisory</lable>
+            </div>
         </div>
     </form>
 </template>
