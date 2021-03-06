d2db9df3f1f307ca382cf261b1e8072ea197cd2a
Bradley Guthrie <guppy96@gmail.com>
2021-03-06 07:19:37 -0600

    registration form loads

--- coach/src/components/coaches/CoachForm.vue
+++ coach/src/components/coaches/CoachForm.vue
@@ -1,35 +1,62 @@
 <template>
-    <form action="">
+    <form @submit.prevent="submitForm">
         <div class="form-control">
             <label for="firstname">firstName</label>
-            <input type="text" id="firstname"/>
+            <input type="text" id="firstname" v-model.trim="firstName"/>
         </div>
         <div class="form-control">
             <label for="lastname">lastName</label>
-            <input type="text" id="lastname"/>
+            <input type="text" id="lastname" v-model.trim="lastName"/>
         </div>
         <div class="form-control">
             <label for="description">Description</label>
-            <textarea id="description" rows="5"></textarea>
+            <textarea id="description" rows="5" v-model.trim="description"></textarea>
         </div>
          <div class="form-control">
             <label for="rate">Hourly Rate</label>
-            <input type="number" id="rate"/>
+            <input type="number" id="rate" v-model.number="rate"/>
         </div>
         <div class="form-control">
             <h3>Areas of Expertise</h3>
             <div>
-                <input type="checkbox" id="frontend" value="frontend">
+                <input type="checkbox" id="frontend" value="frontend" v-model="areas">
                 <lable for="frontend">Frontend Development</lable>
             </div>
              <div>
-                <input type="checkbox" id="backend" value="backend">
+                <input type="checkbox" id="backend" value="backend" v-model="areas">
                 <lable for="backend">Backend Development</lable>
             </div>
              <div>
-                <input type="checkbox" id="career" value="career">
+                <input type="checkbox" id="career" value="career" v-model="areas">
                 <lable for="career">Career Advisory</lable>
             </div>
         </div>
+        <base-button>Register</base-button>
     </form>
 </template>
+
+<script>
+export default {
+    data() {
+        return{
+            firstName: '',
+            lastName: '',
+            description: '',
+            rate: null,
+            areas:[]
+        }
+    },
+    methods:{
+        submitForm() {
+            const formData ={
+                first: this.firstName,
+                last: this.lastName,
+                desc: this.description,
+                rate: this.rate,
+                areas: this.areas
+            };
+            console.log(formData);
+        }
+    }
+}
+</script>
--- coach/src/pages/coaches/CoachRegistration.vue
+++ coach/src/pages/coaches/CoachRegistration.vue
@@ -1,5 +1,15 @@
 <template>
     <section>
         <h2>Register as a coach now</h2>
+        <coach-form></coach-form>
     </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue'
+export default {
+    components: {
+        CoachForm
+    }
+}
+</script>
