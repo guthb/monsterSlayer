0b29a1ef43d3e1f39084afb8ae4ed4e8a582c7b8
Bradley Guthrie <guppy96@gmail.com>
2021-03-07 14:28:47 -0600

    corrected Registration and nowcan register a coach

--- coach/src/store/modules/coaches/actions.js
+++ coach/src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
     registerCoach(context, data) {
         const coachData = {
-            id: context.rootgetters.userId,
+            id: context.rootGetters.userId,
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
