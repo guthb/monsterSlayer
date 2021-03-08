04cb22af5e01c47f86c1243de0e8486e15243a38
Bradley Guthrie <guppy96@gmail.com>
2021-03-08 07:50:42 -0600

    form validation kinda works

--- coach/src/components/coaches/CoachForm.vue
+++ coach/src/components/coaches/CoachForm.vue
@@ -1,36 +1,42 @@
 <template>
     <form @submit.prevent="submitForm">
-        <div class="form-control">
+        <div class="form-control" :class="{invalid: !firstName.isValid}">
             <label for="firstname">First Name</label>
-            <input type="text" id="firstname" v-model.trim="firstName"/>
+            <input type="text" id="firstname" v-model.trim="firstName.val"/>
+            <p v-if="!firstName.isValid">Firstname must not be empty</p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{invalid: !lastName.isValid}">
             <label for="lastname">Last Name</label>
-            <input type="text" id="lastname" v-model.trim="lastName"/>
+            <input type="text" id="lastname" v-model.trim="lastName.val"/>
+             <p v-if="!lastName.isValid">Lastname must not be empty</p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{invalid: !description.isValid}">
             <label for="description">Description</label>
-            <textarea id="description" rows="5" v-model.trim="description"></textarea>
+            <textarea id="description" rows="5" v-model.trim="description.val"></textarea>
+             <p v-if="!description.isValid">Description must not be empty</p>
         </div>
-         <div class="form-control">
+         <div class="form-control" :class="{invalid: !rate.isValid}">
             <label for="rate">Hourly Rate</label>
-            <input type="number" id="rate" v-model.number="rate"/>
+            <input type="number" id="rate" v-model.number="rate.val"/>
+             <p v-if="!rate.isValid">Rate must be greater than 0</p>            
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{invalid: !areas.isValid}">
             <h3>Areas of Expertise</h3>
             <div>
-                <input type="checkbox" id="frontend" value="frontend" v-model="areas">
+                <input type="checkbox" id="frontend" value="frontend" v-model="areas.val">
                 <label for="frontend">Frontend Development</label>
             </div>
              <div>
-                <input type="checkbox" id="backend" value="backend" v-model="areas">
+                <input type="checkbox" id="backend" value="backend" v-model="areas.val">
                 <label for="backend">Backend Development</label>
             </div>
              <div>
-                <input type="checkbox" id="career" value="career" v-model="areas">
+                <input type="checkbox" id="career" value="career" v-model="areas.val">
                 <label for="career">Career Advisory</label>
             </div>
+            <p v-if="!areas.isValid">At least one expertise must be selected</p>
         </div>
+        <p v-if="!formIsValid">Please fix the above errors and submit again</p>
         <base-button>Register</base-button>
     </form>
 </template>
@@ -60,22 +66,47 @@ export default {
               val:[],
               isValid:true
             },
+            formIsValid: true
         }
     },
     methods:{
         vaildateForm() {
-
+            this.formIsValid = true;
+            if ( this.firstName.val === '') {
+              this.firstName.isValid = false;
+              this.formIsValid = false;
+            }
+            if ( this.lastName.val === '') {
+              this.lastName.isValid = false;
+              this.formIsValid = false;
+            }
+            if ( this.description.val === '') {
+              this.description.isValid = false;
+              this.formIsValid = false;
+            }
+            if ( !this.rate.val || this.rate.val < 0) {
+              this.rate.isValid = false;
+              this.formIsValid = false;
+            }
+            if( this.areas.val === 0) {
+              this.areas.isValid = false;
+              this.formIsValid = false;
+            }
         },
         submitForm() {
           this.vaildateForm();
 
-          const formData ={
+          if (!this.formIsValid){
+            return;
+          }
+
+          const formData = {
               first: this.firstName,
               last: this.lastName,
               desc: this.description,
               rate: this.rate,
               areas: [],
-              formIsValid: true
+              
           };
           console.log(formData);
 
