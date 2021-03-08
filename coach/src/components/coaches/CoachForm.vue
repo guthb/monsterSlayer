a409d0e5d1f48fbc1e82281f076946f49ea71484
Bradley Guthrie <guppy96@gmail.com>
2021-03-08 06:23:03 -0600

    form vaildation started

--- coach/src/components/coaches/CoachForm.vue
+++ coach/src/components/coaches/CoachForm.vue
@@ -2,32 +2,32 @@
     <form @submit.prevent="submitForm">
         <div class="form-control">
             <label for="firstname">First Name</label>
-            <input type="text" id="firstname" v-model.trim="firstName"/>
+            <input type="text" id="firstname" v-model.trim="firstName.val"/>
         </div>
         <div class="form-control">
             <label for="lastname">Last Name</label>
-            <input type="text" id="lastname" v-model.trim="lastName"/>
+            <input type="text" id="lastname" v-model.trim="lastName.val"/>
         </div>
         <div class="form-control">
             <label for="description">Description</label>
-            <textarea id="description" rows="5" v-model.trim="description"></textarea>
+            <textarea id="description" rows="5" v-model.trim="description.val"></textarea>
         </div>
          <div class="form-control">
             <label for="rate">Hourly Rate</label>
-            <input type="number" id="rate" v-model.number="rate"/>
+            <input type="number" id="rate" v-model.number="rate.val"/>
         </div>
         <div class="form-control">
             <h3>Areas of Expertise</h3>
             <div>
-                <input type="checkbox" id="frontend" value="frontend" v-model="areas">
+                <input type="checkbox" id="frontend" value="frontend" v-model="areas.val">
                 <label for="frontend">Frontend Development</label>
             </div>
              <div>
-                <input type="checkbox" id="backend" value="backend" v-model="areas">
+                <input type="checkbox" id="backend" value="backend" v-model="areas.val">
                 <label for="backend">Backend Development</label>
             </div>
              <div>
-                <input type="checkbox" id="career" value="career" v-model="areas">
+                <input type="checkbox" id="career" value="career" v-model="areas.val">
                 <label for="career">Career Advisory</label>
             </div>
         </div>
@@ -48,26 +48,47 @@ export default {
         }
     },
     methods:{
-        submitForm() {
-            const formData ={
-                first: this.firstName,
-                last: this.lastName,
-                desc: this.description,
-                rate: this.rate,
-                areas: this.areas
-            };
-            console.log(formData);
+        vaildateForm() {
 
-            this.$emit('save-data', formData)
-        }
-    }
+        },
+        submitForm() {
+          this.vaildateForm();
+
+          const formData ={
+              first: {
+                val: '',
+                isVaild: true
+              },
+              last: {
+                val: '',
+                isVaild: true
+              },
+              desc: {
+                val: '',
+                isVaild: true
+                },
+              rate:{
+                val: null,
+                isVaild: true
+              },
+              areas: {
+                val: [],
+                isVaild: true
+              },
+              formIsValid: true
+          };
+          console.log(formData);
+
+          this.$emit('save-data', formData)
+      }
+  }
 }
 </script>
 
 <style scoped>
 
 .form-control {
-  margin: 0.5rem 0;
+margin: 0.5rem 0;
 }
 
 label {
