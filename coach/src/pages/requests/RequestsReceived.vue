8314c699ec322d7c1982ee5717e18f1dc41a3f45
Bradley Guthrie <guppy96@gmail.com>
2021-03-10 06:53:45 -0600

    getters and index updated, and request item component added

--- coach/src/pages/requests/RequestsReceived.vue
+++ coach/src/pages/requests/RequestsReceived.vue
@@ -4,14 +4,21 @@
     <header>
       <h2>Requests Received</h2>
     </header>   
-    <ul v-if="hasRequests"></ul> 
+    <ul v-if="hasRequests">
+      <request-item v-for="req in receivedRequests" :key="req.id" :email="req.userEmail" :message="req.message"></request-item>
+    </ul> 
     <h3 v-else>You haven't received any requests yet!</h3>
     </base-card>
   </section>
 </template>
 
 <script>
+import RequestItem from '../../components/requests/RequestItem.vue'
+
 export default {
+  components: {
+    RequestItem
+  },
   computed: {
     receivedRequest() {
       return this.$store.getters['requests/requests'];
