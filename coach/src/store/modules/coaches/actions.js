fd1c7971e5b7b84a00f0798ff269274e1a7902fe
Bradley Guthrie <guppy96@gmail.com>
2021-03-12 06:40:12 -0600

    setting up for coach storage

--- coach/src/store/modules/coaches/actions.js
+++ coach/src/store/modules/coaches/actions.js
@@ -1,7 +1,8 @@
 export default {
     registerCoach(context, data) {
+        const userId = context.rootGetters.userId;
         const coachData = {
-            id: context.rootGetters.userId,
+            //id: context.rootGetters.userId,
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
@@ -9,6 +10,20 @@ export default {
             areas: data.areas
         };
 
-        context.commit('registerCoach', coachData);
+        const response = await fetch(`https://vue-coach-566b4-default-rtdb.firebaseio.com/caoches/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData)
+        });
+
+        //const responseData = await response.json();
+
+        if (!response.ok) {
+            // error...
+        }
+
+        context.commit('registerCoach', {
+            ...coachData,
+            id: userId
+        });
     }
 };
