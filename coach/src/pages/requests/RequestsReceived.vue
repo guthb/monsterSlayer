6c51ba3b093977cfff6bf08a4038eaf5003d4281
Bradley Guthrie <guppy96@gmail.com>
2021-03-13 09:10:58 -0600

    hunting for the issue

--- coach/src/pages/requests/RequestsReceived.vue
+++ coach/src/pages/requests/RequestsReceived.vue
@@ -5,7 +5,10 @@
       <h2>Requests Received</h2>
     </header>   
     <ul v-if="hasRequests">
-      <request-item v-for="req in receivedRequests" :key="req.id" :email="req.userEmail" :message="req.message"></request-item>
+      <request-item v-for="req in receivedRequests" 
+      :key="req.id" :email="req.userEmail" 
+      :message="req.message"
+      ></request-item>
     </ul> 
     <h3 v-else>You haven't received any requests yet!</h3>
     </base-card>
