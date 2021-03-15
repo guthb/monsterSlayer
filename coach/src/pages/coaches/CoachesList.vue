0bae3d9cebd27b6ad507a0b119c8d12a3e033c80
Bradley Guthrie <guppy96@gmail.com>
2021-03-15 07:03:37 -0500

    load coaches is not displaying

--- coach/src/pages/coaches/CoachesList.vue
+++ coach/src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
     <section>
         <base-card>
             <div class="controls">
-                <base-button mode= "outline"> Refresh</base-button>
+                <base-button mode= "outline" @click="loadCoaches"> Refresh</base-button>
                 <base-button v-if='!isCoach' link to="/register">Register as Coach</base-button>
             </div>
             <ul v-if="hasCoaches">
@@ -67,14 +67,21 @@ export default {
         },
         hasCoaches() {
             return this.$store.getters['coaches/hasCoaches'];
-        }
+        },
+        
     },
+    created() {
+            this.loadCoaches();
+        },
     methods: {
         setFilters(updatedFilters){
             this.activeFilters = updatedFilters;
            
+        },
+        loadCoaches(){
+            this.$store.dispatch('coaches/loadCoaches');
         }
-    }
+    },
 };
 </script>
 
