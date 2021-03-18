49277b6d3ddd5cebd3750163023aa98892f9913b
Bradley Guthrie <guppy96@gmail.com>
2021-03-18 08:41:48 -0500

    sending to firebase

--- coach/src/store/modules/requests/actions.js
+++ coach/src/store/modules/requests/actions.js
@@ -1,11 +1,25 @@
 export default {
-    contactCoach(context, payload){
+    async contactCoach(context, payload) {
         const newRequest = {
-            id: new Date().toISOString(),
+            //id: new Date().toISOString(),
             coachId: payload.coachId,
             userEmail: payload.message,
             message: payload.message
         };
+        const response = await fetch(`https://vue-coach-adb56-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`{
+            method: 'POST',
+            body: JSON.stringify(newRequest)
+        });
+
+        const responseData = await response.json();
+
+         if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to send request!')
+            throw error;
+         }
+        
+        newRequest.id = responseData.name;
+
         context.commit('addRequest', newRequest);
     }
-}
+};
