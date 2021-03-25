4b6326f0c544751ce0c76e474673c0bfdc59ec9a
bradley guthrie <guppy96@gmail.com>
2021-03-25 16:51:35 -0500

    route added

--- auth/src/pages/auth/UserAuth.vue
+++ auth/src/pages/auth/UserAuth.vue
@@ -1,23 +1,23 @@
 <template>
+    <base-card>
     <form>
-        <div class="form-control">
-            <label for="email">E-mail</label>
-            <input type="email" id="email">
-        </div>
-        <div class="form-control">
-            <label for="password">Password</label>
-            <input type="password" id='password'>
-        </div>
-        <base-button>Login</base-button>
-        <base-button type="button" mode="flat">Signup instead</base-button>
+    <div class="form-control">
+        <label for="email">E-mail</label>
+        <input type="email" id="email">
+    </div>
+    <div class="form-control">
+        <label for="password">Password</label>
+        <input type="password" id='password'>
+    </div>
+    <base-button>Login</base-button>
+    <base-button type="button" mode="flat">Signup instead</base-button>
     </form>
+    </base-card>
 </template>
 
 <style scoped>
 form {
   margin: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 12px;
   padding: 1rem;
 }
 
