790af4abbfb69cdbe0ee69b43812131832c22381
bradley guthrie <guppy96@gmail.com>
2021-03-27 17:08:27 -0500

    starting again

--- auth/src/store/modules/auth/action.js
+++ auth/src/store/modules/auth/action.js
@@ -1,1 +1,23 @@
-export default {}
+export default {
+    login() { },
+    async signup(context, payload) {
+        const response = await fetch(`{
+            method: 'POST',
+            body: JSON.stringify({
+                email: payload.eamil,
+                password: payload.password,
+                returnSecureToken:true
+            })
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            console.log(resonseData)
+            const error = new Error(resonseData.message || 'Failed to authenticate');
+            throw error;
+        }
+
+        console.log(resonseData);
+    }
+}
