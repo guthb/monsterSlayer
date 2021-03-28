dc06bfb513aca722724a124ceae47b7100f8892b
bradley guthrie <guppy96@gmail.com>
2021-03-28 11:29:13 -0500

    ready to test with key

--- auth/src/store/modules/auth/action.js
+++ auth/src/store/modules/auth/action.js
@@ -1,7 +1,7 @@
 export default {
-    login() { },
+    login() {},
     async signup(context, payload) {
-        const response = await fetch(`{
+        const response = await fetch(`[enter api key to test]`,{
             method: 'POST',
             body: JSON.stringify({
                 email: payload.eamil,
@@ -13,11 +13,16 @@ export default {
         const responseData = await response.json();
 
         if (!response.ok) {
-            console.log(resonseData)
+            console.log("not ok response",resonseData)
             const error = new Error(resonseData.message || 'Failed to authenticate');
             throw error;
         }
 
         console.log(resonseData);
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+            tokenExpiration: responseData.expiresIn
+        });
     }
 }
--- auth/src/store/modules/auth/getters.js
+++ auth/src/store/modules/auth/getters.js
@@ -1,15 +1,5 @@
-import mutations from './mutations.js';
-import getters from './getters.js';
-import actions from './action.js';
-
-
 export default {
-    state() {
-    return {
-      userId: 'c3'
-    };
-    },
-    mutations,
-    actions,
-    getters
-}
+  userId(state) {
+    return state.userId;
+  }
+};
