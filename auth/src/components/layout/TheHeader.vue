ef41b11c32ac048a6d4e7d09810bc54ed6961641
bradley guthrie <guppy96@gmail.com>
2021-04-02 06:54:02 -0500

    added logout button

--- auth/src/components/layout/TheHeader.vue
+++ auth/src/components/layout/TheHeader.vue
@@ -8,13 +8,31 @@
         <li>
           <router-link to="/coaches">All Coaches</router-link>
         </li>
-        <li>
+        <li v-if="isLoggedIn">
           <router-link to="/requests">Requests</router-link>
+        </li>
+        <li v-else>
+          <router-link to="/auth">Login</router-link>
+        </li>
+        <li v-if="isLoggedIn">
+          <base-button>Logout</base-button>
         </li>
       </ul>
     </nav>
   </header>
 </template>
+
+<script>
+export default {
+  computed: {
+    isLoggedIn()
+    {
+      return this.$store.getters.isAuthenticated;
+    }
+  }
+}
+</script>
+
 
 <style scoped>
 header {
