700277d2c01312a589ee129d24fd92d7c2e6ff61
bradley guthrie <guppy96@gmail.com>
2021-04-13 06:48:42 -0500

    complete goal 1

--- compositionapi/effort/src/App.vue
+++ compositionapi/effort/src/App.vue
@@ -2,7 +2,7 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3>{{goal}}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
   <button>Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
@@ -14,7 +14,11 @@
 
 <script>
 export default {
-  
+  setup() {
+    const courseGoal = 'Finish comp api effort';
+
+    return { goal: courseGoal };
+  }
 }
 </script>
 
