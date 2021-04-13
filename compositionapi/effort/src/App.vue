9f0785b2cf457bd87a6ccca5f5e83b7c15beca76
bradley guthrie <guppy96@gmail.com>
2021-04-13 07:38:32 -0500

    ref object implemented

--- compositionapi/effort/src/App.vue
+++ compositionapi/effort/src/App.vue
@@ -2,7 +2,7 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3 v-if="goalIsVisible">{{goal}}</h3>
+  <h3 v-if="courseData.goalVisibility">{{goal}}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
   <button @click="togggleGoalVisibility">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
@@ -16,17 +16,22 @@
 import {ref} from 'vue';
 export default {
   setup() {
-    const courseGoal = ref('Finish comp api effort');
-    const goalIsVisible = reg(false);
+    // const courseGoal = ref('Finish comp api effort');
+    // const goalIsVisible = reg(false);
+    const courseData = ref({
+      goal: "another way",
+      goalVisibility: false
+    })
 
 
     function toggleGoalVisibility() {
-      goalIsVisibl.value = !goalIsVisible.value;
+      //goalIsVisible.value = !goalIsVisible.value;
+      courseData.value.goalVisibility = !courseData.value.goalVisibility;
     }
 
     return { 
-      goal: courseGoal, 
-      goalIsVisible,
+      goal: courseGoal.value.goal, 
+      courseData.
       toggleGoalVisibility };
   }
 }
