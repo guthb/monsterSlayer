15a18478ccfcfebad7b0518ad91b828202812e3f
bradley guthrie <guppy96@gmail.com>
2021-04-18 09:31:41 -0500

    checking that entries do reduce the value

--- compositionapi/moneytracker/src/App.vue
+++ compositionapi/moneytracker/src/App.vue
@@ -25,7 +25,7 @@ import {ref, computed } from 'vue';
 
 export default {
   setup() {
-    const availableFunds: 100;
+    const availableFunds=  100;
 
     const currentExpenses = ref(0);
     const enteredExpense = ref(0);
