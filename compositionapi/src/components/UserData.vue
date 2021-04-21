f9235e785f5e4555e7cf12e1efc631403d6e56db
bradley guthrie <guppy96@gmail.com>
2021-04-21 06:50:58 -0500

    looking at life cycle hook

--- compositionapi/src/components/UserData.vue
+++ compositionapi/src/components/UserData.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import { computed, inject } from 'vue';
+import { computed, inject, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated } from 'vue';
 
 export default {
     props: ['firstName', 'lastName', 'age'],
@@ -21,11 +21,40 @@ export default {
         });
 
         const age = inject('userAge')
-
-
-
+        
         console.log(context);
 
+        onBeforeMount(function()
+        {
+          console.log('onBeforeMount');
+        });
+
+        onMounted(function()
+        {
+          console.log('onMounted');
+        });
+
+        onBeforeUpdate(function()
+        {
+          console.log('onBeforeUpate');
+        });
+
+        onUpdated(function()
+        {
+          console.log('onUpdated');
+        });
+
+        onBeforeUnmount(function()
+        {
+          console.log('onBeforeUnmount');
+        });
+
+        onUnmounted(function()
+        {
+          console.log('onUnmounted');
+        });
+        
+        
         return { userName: uName, age };
     }
 }
