5d52fe4da8c814e2ede86f88881df28d9fd0b9c3
bradley guthrie <guppy96@gmail.com>
2021-04-28 05:54:30 -0500

    added store testing

--- counter/src/components/TheCounter.vue
+++ counter/src/components/TheCounter.vue
@@ -3,8 +3,18 @@
 </template>
 
 <script>
+import { computed } from 'vue'
+import {useStore} from 'vuex';
+
 export default {
   setup() {
+
+    const store = useStore();
+
+    const counter = computed(function() {
+      return store.getters.counter;
+    });
+
     const counter = 0;
 
     return { counter };
