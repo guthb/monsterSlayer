06ba2a44a93e33bd0996efa7dfcb52cb1330d666
bradley guthrie <guppy96@gmail.com>
2021-05-11 08:35:29 -0500

    more changes still will not start server

--- vue3demo/router-v3/src/App.vue
+++ vue3demo/router-v3/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <the-header></the-header>
-      <router-view>
-        <transition name="route" mode="out-in"></transition>
+      <router-view v-slot="slotProps">
+        <transition name="route" mode="out-in">
+          <component :is="slotProps.component"></component>
+        </transition>
       </router-view>
   </div>
 </template>
@@ -38,7 +40,7 @@ body {
   border-radius: 12px;
 }
 
-.route-enter,
+.route-enter-from,
 .route-leave-to {
   opacity: 0;
   transform: translateY(-30px);
@@ -50,7 +52,7 @@ body {
 }
 
 .route-enter-to,
-.route-leave {
+.route-leave-from {
   opacity: 1;
   transform: translateY(0);
 }
