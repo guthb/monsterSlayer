62e07bcf32186122a6bee0aa6fc3fa68fae53b60
bradley guthrie <guppy96@gmail.com>
2021-05-11 08:27:49 -0500

    changes made server will not start

--- vue3demo/router-v3/src/main.js
+++ vue3demo/router-v3/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import { createRouter } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue';
 import WelcomeScreen from './pages/WelcomeScreen.vue';
